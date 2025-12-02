d1609d6a87c4fd40a6023e03a1013af0d922253e
EliasF <semeaoelias@gmail.com>
2025-12-02 13:19:32 -0300

    CORS update

--- src/main/java/com/eliasfilipe/gamelist/config/WebConfig.java
+++ src/main/java/com/eliasfilipe/gamelist/config/WebConfig.java
@@ -1,16 +1,26 @@
 package com.eliasfilipe.gamelist.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(corsOrigins.split(","))
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
-
 }
